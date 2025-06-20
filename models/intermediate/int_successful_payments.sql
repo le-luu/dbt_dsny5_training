c55aa4ea67f3a1c2e2c64c03f0eb798e2721b1ac
Le Luu <le.luu.dev@gmail.com>
2025-06-20 18:37:31 +0000

    Add customer model

---
+++ models/intermediate/int_successful_payments.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized='ephemeral'
+    )
+}}
+
+WITH payments as 
+(
+    SELECT *
+    FROM {{ ref('stg_stripe__payment') }}
+),
+successful as (
+    SELECT *
+    FROM payments
+    WHERE status='success'
+)
+SELECT *
+FROM successful
